90c26782b923ff5b3394269c0eb160c4f2483955
Artem <artemisuzl@gmail.com>
2021-07-21 22:22:01 +0300

    update to 5 lesson

---
+++ app/src/main/java/ru/geekbrains/geekbrains_popular_libraries_kotlin/mvp/view/UsersView.kt
@@ -0,0 +1,13 @@
+package ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.view
+
+import moxy.MvpView
+import moxy.viewstate.strategy.alias.AddToEndSingle
+
+@AddToEndSingle
+interface UsersView : MvpView {
+
+    fun init()
+    fun updateList()
+    fun showErrorMessage(message: String)
+
+}
